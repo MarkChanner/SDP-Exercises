72e89ed8f1846bc36c7d466b5c04e393c0e8c0ed
markchanner <markchanner@gmail.com>
2016-01-14 11:52:43 +0000

    First commit

---
+++ SDP-wk01-reflection/src/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+
+    public static void main(String[] args) {
+        Storage<BankAccount> aStorage = new Storage<>();
+        Storage<String> sStorage = new Storage<>();
+    }
+}
