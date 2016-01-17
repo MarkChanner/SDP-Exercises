13a6ae91f7d8dffeaac355e6ae115a7e69df28d9
markchanner <markchanner@gmail.com>
2016-01-17 20:08:29 +0000

    Adds comment regarding Singleton pattern usage

--- SDP-wk02-di/DecoupledWithFactory/src/decoupledwithfactory/HelloWorldDecoupledWithFactory.java
+++ SDP-wk02-di/DecoupledWithFactory/src/decoupledwithfactory/HelloWorldDecoupledWithFactory.java
@@ -3,7 +3,8 @@ package decoupledwithfactory;
 public class HelloWorldDecoupledWithFactory {
 
     public static void main(String[] args) {
-
+        // getInstance() is part of Singleton pattern, enabling MessageSupportFactory to
+        // return a single reference of itself, which getMessageRenderer() is then called on
         MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer();
         MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider();
         mr.setMessageProvider(mp);
