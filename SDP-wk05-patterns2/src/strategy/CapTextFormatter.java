719b8b7efa5fc846c167570b4d8e7c5fa224250b
markchanner <markchanner@gmail.com>
2016-02-04 19:22:03 +0000

    Adds text to output of TextFormatter implementations

--- SDP-wk05-patterns2/src/strategy/CapTextFormatter.java
+++ SDP-wk05-patterns2/src/strategy/CapTextFormatter.java
@@ -8,6 +8,6 @@ package strategy;
 public class CapTextFormatter implements TextFormatter {
 
     public void format(String text) {
-        System.out.println(text.toUpperCase());
+        System.out.println("[CapTextFormatter] " + text.toUpperCase());
     }
 }
--- SDP-wk05-patterns2/src/strategy/LowerTextFormatter.java
+++ SDP-wk05-patterns2/src/strategy/LowerTextFormatter.java
@@ -8,6 +8,6 @@ package strategy;
 public class LowerTextFormatter implements TextFormatter {
 
     public void format(String text) {
-        System.out.println(text.toLowerCase());
+        System.out.println("[LowerTextFormatter] " + text.toLowerCase());
     }
 }
