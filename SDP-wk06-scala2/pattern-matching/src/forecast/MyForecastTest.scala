327af2d4651b3eae7cb21b9a060971202a942644
markchanner <markchanner@gmail.com>
2016-02-10 23:11:10 +0000

    First exercise done

--- SDP-wk06-scala2/pattern-matching/src/forecast/MyForecastTest.scala
+++ SDP-wk06-scala2/pattern-matching/src/forecast/MyForecastTest.scala
@@ -1,9 +1,13 @@
 package forecast
 
 import atomicscala.AtomicTest._
+
 /**
-  * Created by Mark on 10/02/2016.
+  * SDP Scala Exercises Week 6
+  * Pattern Matching Q.1
+  * Mark Channer
   */
+
 object MyForecastTest extends App {
   forecast(100) is "Sunny"
   forecast(80) is "Mostly Sunny"
@@ -12,5 +16,14 @@ object MyForecastTest extends App {
   forecast(0) is "Cloudy"
   forecast(15) is "Unknown"
 
-  def forecast(temp: Int): String = ???
+  def forecast(temp: Int): String = {
+    temp match {
+      case 100 => "Sunny"
+      case 80 => "Mostly Sunny"
+      case 50 => "Partly Sunny"
+      case 20 => "Mostly Sunny"
+      case 0 => "Cloudy"
+      case _ => "Unknown"
+    }
+  }
 }
