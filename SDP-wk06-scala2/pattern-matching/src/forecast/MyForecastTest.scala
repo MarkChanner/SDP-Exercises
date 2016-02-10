75931ba9bbaa328b1e6b934067c8383866a656ad
markchanner <markchanner@gmail.com>
2016-02-10 22:51:24 +0000

    First commit

--- SDP-wk06-scala2/pattern-matching/src/forecast/MyForecastTest.scala
+++ SDP-wk06-scala2/pattern-matching/src/forecast/MyForecastTest.scala
@@ -1,8 +1,16 @@
 package forecast
 
+import atomicscala.AtomicTest._
 /**
   * Created by Mark on 10/02/2016.
   */
-object MyForecastTest {
+object MyForecastTest extends App {
+  forecast(100) is "Sunny"
+  forecast(80) is "Mostly Sunny"
+  forecast(50) is "Partly Sunny"
+  forecast(20) is "Mostly Sunny"
+  forecast(0) is "Cloudy"
+  forecast(15) is "Unknown"
 
+  def forecast(temp: Int): String = ???
 }
