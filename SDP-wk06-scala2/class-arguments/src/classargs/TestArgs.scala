ae9bb38e5883a3785bb997b029590d3ce905a081
markchanner <markchanner@gmail.com>
2016-02-11 20:33:35 +0000

    Finished Questions 4-5

--- SDP-wk06-scala2/class-arguments/src/classargs/TestArgs.scala
+++ SDP-wk06-scala2/class-arguments/src/classargs/TestArgs.scala
@@ -8,17 +8,38 @@ import atomicscala.AtomicTest._
   * Mark Channer
   */
 
-class Family(val members : String*) {
+class Family(val members: String*) {
 
   def familySize() = members.size
 
 }
 
+class FlexibleFamily(val mum: String, val dad: String, val children: String*) {
+  def familySize() = children.size + 2
+}
+
 object TestArgs extends App {
+  // Question 3
   val family1 = new Family("Mum", "Dad", "Sally", "Dick")
   family1.familySize() is 4
-
   val family2 = new Family("Dad", "Mom", "Harry")
   family2.familySize() is 3
 
+  // Question 4
+  val family3 = new Family("Mum", "Dad", "Sally2", "Dick")
+  family3.familySize() is 4
+  val family4 = new Family("Mum", "Dad", "Harry")
+  family4.familySize() is 3
+
+  // Question 5
+  def squareThem(values: Int*): Int = {
+    var result = 0
+    for (v <- values) {
+      result += v * v
+    }
+    result
+  }
+  squareThem(2) is 4
+  squareThem(2, 4) is 20
+  squareThem(1, 2, 4) is 21
 }
