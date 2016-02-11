7b118659c36e89ef53654b9e46b861a7dfb5aab9
markchanner <markchanner@gmail.com>
2016-02-11 21:09:21 +0000

    Finished Questioons 9

--- SDP-wk06-scala2/named-and-default-arguments/src/simpletime/SimpleTimeTest.scala
+++ SDP-wk06-scala2/named-and-default-arguments/src/simpletime/SimpleTimeTest.scala
@@ -31,5 +31,9 @@ object TestSimpleTime extends App {
   // Question 8
   val p = new Planet(name = "Mercury", description = "small and hot planet", moons = 0)
   p.hasMoon is false
+
+  // Question 9. Shows how the argument order can be mixed
+  val earth = new Planet(moons = 1, name = "Earth", description = "a hospitable planet")
+  earth.hasMoon is true
 }
 
