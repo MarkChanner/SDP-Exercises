317c9500e906f6f4944f9143efe9484e414e7789
markchanner <markchanner@gmail.com>
2016-02-12 00:38:26 +0000

    Finished Question 10

--- SDP-wk06-scala2/named-and-default-arguments/src/simpletime/SimpleTimeTest.scala
+++ SDP-wk06-scala2/named-and-default-arguments/src/simpletime/SimpleTimeTest.scala
@@ -17,6 +17,17 @@ class Planet(val name: String, val description: String, val moons: Int = 1) {
   def hasMoon: Boolean = moons != 0
 }
 
+class Item(val name: String, val price: Double) {
+
+  def cost(grocery: Boolean = false, medication: Boolean = false, taxRate: Double = 0.10) = {
+    var result = price
+    if (!grocery && !medication) {
+      result += (price * taxRate)
+    }
+    result
+  }
+}
+
 object TestSimpleTime extends App {
   // Question 6
   val t = new SimpleTime(hours = 5, minutes = 30)
@@ -35,5 +46,13 @@ object TestSimpleTime extends App {
   // Question 9. Shows how the argument order can be mixed
   val earth = new Planet(moons = 1, name = "Earth", description = "a hospitable planet")
   earth.hasMoon is true
+
+  // Question 10
+  val flour = new Item(name = "flour", 4)
+  flour.cost(grocery = true) is 4
+  val sunscreen = new Item(name = "sunscreen", 3)
+  sunscreen.cost() is 3.3
+  val tv = new Item(name = "television", 500)
+  tv.cost(taxRate = 0.06) is 530
 }
 
