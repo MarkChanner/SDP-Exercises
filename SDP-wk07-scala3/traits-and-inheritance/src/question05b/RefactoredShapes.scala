1da0734845c9509f268a2a9858e4024962df8bda
markchanner <markchanner@gmail.com>
2016-02-21 17:01:51 +0000

    Finishes Q.5b

--- SDP-wk07-scala3/traits-and-inheritance/src/question05b/RefactoredShapes.scala
+++ SDP-wk07-scala3/traits-and-inheritance/src/question05b/RefactoredShapes.scala
@@ -9,47 +9,42 @@ package question05b
   * @author Mark Channer
   */
 /**
-(b) The solution from part a delivered three distinct types of shape. However, it
-  doesn’t model the relationships between the three correctly. A Square isn’t just
-  a Shape –– it’s also a type of Rectangle where the width and height are the
-  same.
-  We want to avoid case-class-to-case-class inheritance, so refactor the solution to
-  the last exercise so that Square and Rectangle are subtypes of a common type
-  Rectangular.
+  * (b) The solution from part a delivered three distinct types of shape. However, it
+  * doesn’t model the relationships between the three correctly. A Square isn’t just
+  * a Shape –– it’s also a type of Rectangle where the width and height are the
+  * same.
+  * We want to avoid case-class-to-case-class inheritance, so refactor the solution to
+  * the last exercise so that Square and Rectangle are subtypes of a common type
+  * Rectangular.
   */
 sealed trait Shape {
-
   def sides: Int
-
   def perimeter: Double
-
   def area: Double
-}
-
-sealed trait Rectangular extends Shape {
-
 }
 
 final case class Circle(radius: Double) extends Shape {
   def sides = 1
   val perimeter = (math.Pi * 2) * radius
   val area = (math.Pi * radius) * radius
-
 }
 
-final case class Rectangle(length: Double, height: Double) extends Shape {
-  val sides = 4
+sealed trait Rectangular extends Shape {
+  def length: Double
+  def height: Double
+  def sides: Int = 4
   def perimeter = (length * 2) + (height * 2)
-  val area = length * height
+  def area = length * height
 }
 
-final case class Square(size: Double) extends Shape {
-  val sides = 4
-  val perimeter = size * 4
-  def area = size * size
+final case class Rectangle(length: Double, height: Double) extends Rectangular
+
+final case class Square(size: Double) extends Rectangular {
+  val length = size
+  val height = size
 }
 
-object testShapes {
+object testRefactoredShapes {
   def main(args: Array[String]): Unit = {
     val circle: Shape = Circle(12)
     assert(circle.sides == 1)
@@ -63,8 +58,7 @@ object testShapes {
 
     val square: Shape = Square(5)
     assert(square.sides == 4)
-    assert(square.perimeter == 20 )
+    assert(square.perimeter == 20)
     assert(square.area == 25)
-
   }
 }
