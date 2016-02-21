c761e977779541c7bc8e4f31011d6a806d2046b8
markchanner <markchanner@gmail.com>
2016-02-21 16:00:21 +0000

    Finishes Q.5a

--- SDP-wk07-scala3/traits-and-inheritance/src/question05/Shapes.scala
+++ SDP-wk07-scala3/traits-and-inheritance/src/question05/Shapes.scala
@@ -2,30 +2,56 @@ package question05
 
 /**
   * SDP Scala Exercises Week 7
-  * Traits and Inheritance Q.5-11
-  * Mark Channer
+  * Traits and Inheritance, Questions 5a & 5b
   *
+  * Note how you can use either val or def to retrieve the result.
+  * This is one way Scala conforms to the Universal Access Principle
+  * Mark Channer
   */
-/**
-  5. (a) Define a trait called Shape and give it three abstract methods:
-  i. 'sides' returns the number of sides;
-  ii. 'perimeter' returns the total length of the sides;
-  iii. 'area' returns the area.
-  Implement 'Shape' with three classes: 'Circle', 'Rectangle', and 'Square'. In each
-  case provide implementations of each of the three methods. Ensure that the
-  main constructor parameters of each shape (e.g., the radius of the circle) are
-  accessible AS FIELDS.
-  Tip: The value of pi is accessible as math.Pi.
- */
-trait Shape {
+sealed trait Shape {
 
   def sides: Int
+
   def perimeter: Double
+
   def area: Double
+}
+
+final case class Circle(radius: Double) extends Shape {
+  def sides = 1
+  val perimeter = (math.Pi * 2) * radius
+  val area = (math.Pi * radius) * radius
 
 }
 
+final case class Rectangle(length: Double, height: Double) extends Shape {
+  val sides = 4
+  def perimeter = (length * 2) + (height * 2)
+  val area = length * height
+}
 
-object main extends App {
+final case class Square(length: Double) extends Shape {
+  val sides = 4
+  val perimeter = length * 4
+  def area = length * length
+}
 
+object testShapes {
+  def main(args: Array[String]): Unit = {
+    val circle: Shape = Circle(12)
+    assert(circle.sides == 1)
+    assert(circle.perimeter == 75.39822368615503)
+    assert(circle.area == 452.3893421169302)
+
+    val rect: Shape = Rectangle(4, 12)
+    assert(rect.sides == 4)
+    assert(rect.perimeter == 32)
+    assert(rect.area == 48)
+
+    val square: Shape = Square(5)
+    assert(square.sides == 4)
+    assert(square.perimeter == 20 )
+    assert(square.area == 25)
+
+  }
 }
