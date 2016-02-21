85cbf0fdb275c61e5a988f69ad8ed180be79be0e
markchanner <markchanner@gmail.com>
2016-02-21 18:41:07 +0000

    Finishes  5d(iii)

--- SDP-wk07-scala3/traits-and-inheritance/src/question5d/Colour.scala
+++ SDP-wk07-scala3/traits-and-inheritance/src/question5d/Colour.scala
@@ -6,13 +6,6 @@ package question5d
   *
   * @author Mark Channer
   */
-/*
-5d) Write a sealed trait Colour to make our shapes more interesting.
-  i. give Colour three properties for its RGB values;
-  ii. create three predefined colours: Red, Yellow, and Pink;
-  iii. provide a means for people to produce their own custom Colours with their
-  own RGB values;
-*/
 sealed trait Colour {
   def red: Int
 
@@ -21,22 +14,26 @@ sealed trait Colour {
   def blue: Int
 }
 
-final case object Red extends Colour {
+case object Red extends Colour {
   val red = 255
   val green = 255
   val blue = 0
 }
 
-final case object Yellow extends Colour {
+case object Yellow extends Colour {
   val red = 255
   val green = 0
   val blue = 0
 }
 
-final case object Pink extends Colour {
+case object Pink extends Colour {
   val red = 255
   val green = 192
   val blue = 203
+}
+
+final case class CustomColour(red: Int, green: Int, blue: Int) extends Colour {
+
 }
 
 
