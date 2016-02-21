fe1a12cb7b329d6efec1daaa3f6ff79bc02d6fd0
markchanner <markchanner@gmail.com>
2016-02-21 17:02:42 +0000

    Removes comment

--- SDP-wk07-scala3/traits-and-inheritance/src/question05b/RefactoredShapes.scala
+++ SDP-wk07-scala3/traits-and-inheritance/src/question05b/RefactoredShapes.scala
@@ -8,15 +8,6 @@ package question05b
   *
   * @author Mark Channer
   */
-/**
-  * (b) The solution from part a delivered three distinct types of shape. However, it
-  * doesn’t model the relationships between the three correctly. A Square isn’t just
-  * a Shape –– it’s also a type of Rectangle where the width and height are the
-  * same.
-  * We want to avoid case-class-to-case-class inheritance, so refactor the solution to
-  * the last exercise so that Square and Rectangle are subtypes of a common type
-  * Rectangular.
-  */
 sealed trait Shape {
   def sides: Int
   def perimeter: Double
