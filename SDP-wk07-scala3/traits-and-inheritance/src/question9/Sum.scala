ab8b3bd1a1ecc6a2cf3bbbe0e45d0fefbece2a18
markchanner <markchanner@gmail.com>
2016-02-24 17:45:21 +0000

    Finishes Q.9

--- SDP-wk07-scala3/traits-and-inheritance/src/question9/Sum.scala
+++ SDP-wk07-scala3/traits-and-inheritance/src/question9/Sum.scala
@@ -7,6 +7,21 @@ package question9
   * @author Mark Channer
   */
 sealed trait Sum[A, B]
-final case class Left[A, B](left: A, right: B) extends Sum
-final case class Right[A, B](left: A, right: B) extends Sum
 
+final case class Left[A, B](value: A) extends Sum[A, B]
+
+final case class Right[A, B](value: B) extends Sum[A, B]
+
+object Main extends App {
+  assert(Left[Int, String](1).value == 1)
+  assert(Right[Int, String]("foo").value == "foo")
+
+  val sum: Sum[Int, String] = Right("foo")
+  assert(sum == Right("foo"))
+
+  val result = sum match {
+    case Left(x) => x.toString
+    case Right(x) => x
+  }
+  assert(result == "foo")
+}
