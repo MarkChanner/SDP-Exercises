55b7f398778f8a031ba3fa11791e3a23d640c19e
markchanner <markchanner@gmail.com>
2016-02-25 10:02:25 +0000

    Finishes Q.1(b) -implements length method

--- SDP-wk08-scala4/generics/src/question01/LinkedList.scala
+++ SDP-wk08-scala4/generics/src/question01/LinkedList.scala
@@ -6,8 +6,22 @@ package question01
   *
   * @author Mark Channer
   */
-sealed trait LinkedList[A]
+
+sealed trait LinkedList[A] {
+  def length: Int =
+    this match {
+      case Pair(head, tail) => 1 + tail.length
+      case End() => 0
+    }
+}
 
 final case class End[A]() extends LinkedList[A]
 
 final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
+
+object Main extends App {
+  val example = Pair(1, Pair(2, Pair(3, End())))
+  assert(example.length == 3)
+  assert(example.tail.length == 2)
+  assert(End().length == 0)
+}
