e25ff2cba2d5e8bf8f6f4d5ccc5ab60a859b189e
markchanner <markchanner@gmail.com>
2016-02-25 10:11:28 +0000

    Finishes Q.1(c) -implements contains method

--- SDP-wk08-scala4/generics/src/question01/LinkedList.scala
+++ SDP-wk08-scala4/generics/src/question01/LinkedList.scala
@@ -13,6 +13,15 @@ sealed trait LinkedList[A] {
       case Pair(head, tail) => 1 + tail.length
       case End() => 0
     }
+
+  def contains(item: A): Boolean = {
+    this match {
+      case Pair(head, tail) =>
+        if (head == item) true
+        else tail.contains(item)
+      case End() => false
+    }
+  }
 }
 
 final case class End[A]() extends LinkedList[A]
@@ -24,4 +33,11 @@ object Main extends App {
   assert(example.length == 3)
   assert(example.tail.length == 2)
   assert(End().length == 0)
+
+  val example2 = Pair(1, Pair(2, Pair(3, End())))
+  assert(example2.contains(3))
+  assert(!example2.contains(4))
+  assert(!End().contains(0))
+  // This should not compile
+  // example.contains("not an Int")
 }
