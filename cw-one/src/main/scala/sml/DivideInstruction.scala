c0ab1bb350fe11aaa897690097b31c9e5f12e8c3
markchanner <markchanner@gmail.com>
2016-03-10 15:14:33 +0000

    Adds tests for executre and toString

--- cw-one/src/main/scala/sml/DivideInstruction.scala
+++ cw-one/src/main/scala/sml/DivideInstruction.scala
@@ -1,8 +1,26 @@
 package sml
 
 /**
-  * Created by Mark on 09/03/2016.
+  * DivideInstruction extends Instruction
+  *
+  * @author Mark Channer
   */
-class DivideInstruction {
+case class DivideInstruction(label: String, op: String, val result: Int, val op1: Int, val op2: Int)
+  extends Instruction(label, op) {
 
+  override def execute(m: Machine) {
+    val value1 = m.regs(op1)
+    val value2 = m.regs(op2)
+    m.regs(result) = value1 / value2
+  }
+
+  override def toString(): String = {
+    super.toString + " " + op1 + " / " + op2 + " to " + result  + "\n"
+  }
 }
+
+object DivideInstruction {
+  def apply(label: String, result: Int, op1: Int, op2: Int) =
+    new DivideInstruction(label, "divide", result, op1, op2)
+}
+
