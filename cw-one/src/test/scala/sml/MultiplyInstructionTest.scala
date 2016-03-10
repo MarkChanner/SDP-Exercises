a75d5ceba8a28d4ea2e33031f0d06e03f9ea0f37
Mark Channer <MarkChanner@users.noreply.github.com>
2016-03-10 15:01:51 +0000

    Adds test for execute and toString

--- cw-one/src/test/scala/sml/MultiplyInstructionTest.scala
+++ cw-one/src/test/scala/sml/MultiplyInstructionTest.scala
@@ -1,8 +1,25 @@
 package sml
 
-/**
-  * Created by Mark on 09/03/2016.
-  */
-class MulInstructionTest {
+import org.scalatest.{Matchers, FunSuite}
 
+/**
+  * Tests MultiplyInstruction.scala
+  *
+  * @author Mark Channer
+  */
+class MultiplyInstructionTest extends FunSuite with Matchers {
+
+  test("Tests MultiplyInstruction initialization and execute method") {
+    val machine = Machine(Labels(), Vector())
+    machine.regs.update(1, 8)
+    machine.regs.update(2, 2)
+    val multiply = MultiplyInstruction("L3", 3, 1, 2)
+    multiply.execute(machine)
+
+    val resultOfExecute = machine.regs(3)
+    resultOfExecute should equal(16)
+
+    val resultOfToString = multiply.toString()
+    resultOfToString should equal("L3: multiply 1 * 2 to 3\n")
+  }
 }
