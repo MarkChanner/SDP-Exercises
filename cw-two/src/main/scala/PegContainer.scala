722f4f98a485d93a6bf4852b30e545c4e0f72332
markchanner <markchanner@gmail.com>
2016-03-16 21:00:58 +0000

    Tests output

---
+++ cw-two/src/main/scala/PegContainer.scala
@@ -0,0 +1,43 @@
+import scala.util.Random
+
+trait PegContainer {
+  def pegs: Vector[Peg]
+
+  override def toString = pegs.mkString("")
+}
+
+case class SecretCode(pegs: Vector[Peg]) extends PegContainer
+
+object SecretCodeGenerator {
+  val CODE_LENGTH = 4
+  val PEG_COLOURS = 6
+
+  def apply(): SecretCode = {
+    run(Vector[Peg]())
+  }
+
+  def run(v: Vector[Peg]): SecretCode = {
+    if (v.size == CODE_LENGTH) SecretCode(v)
+    else {
+      var code = v
+      Random.nextInt(PEG_COLOURS) match {
+        case 0 =>
+          code = code :+ Blue()
+        case 1 =>
+          code = code :+ Green()
+        case 2 =>
+          code = code :+ Orange()
+        case 3 =>
+          code = code :+ Purple()
+        case 4 =>
+          code = code :+ Red()
+        case 5 =>
+          code = code :+ Yellow()
+      }
+      run(code)
+    }
+  }
+
+}
+
+case class Guess(pegs: Vector[Peg]) extends PegContainer
