e07f524ecd102307122e98d38b98ed559ac408be
markchanner <markchanner@gmail.com>
2016-03-16 18:34:02 +0000

    Initial commit.

---
+++ cw-two/src/test/scala/PegTest.scala
@@ -0,0 +1,22 @@
+import org.scalatest.{FunSuite, Matchers}
+
+class PegTest extends FunSuite with Matchers {
+  test("testPegs") {
+    val b: Peg = Blue()
+    b should be(Blue())
+    b.toString should be("B")
+
+    val g: Peg = Green()
+    g should be(Green())
+    g.toString should be("G")
+
+    val black: ResultPeg = Black()
+    black should be (Black())
+    black.toString should be("BLACK")
+
+    val white: ResultPeg = White()
+    white should be(White())
+    white.toString should be("WHITE")
+  }
+}
+
