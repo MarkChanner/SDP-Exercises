4e92f897a625287ba4536e5f586740edd708394a
markchanner <markchanner@gmail.com>
2016-03-19 18:10:21 +0000

    Tidies up

--- cw-two/src/main/scala/Mastermind.scala
+++ cw-two/src/main/scala/Mastermind.scala
@@ -1,19 +1,22 @@
 case class Mastermind(easy: Boolean) extends GameAbstractImpl {
 
-  val MAXATTEMPTS = 12
+  val maxAttempts = 12
 
   def runGames = {
     val board = Board(Vector[Round]())
-    val remainingAttempts = MAXATTEMPTS - board.rounds.length
+    val remainingAttempts = maxAttempts - board.rounds.length
+    val secretCode = SecretCodeGenerator()
 
     Printer(Output.intro)
     Printer(Output.begin)
 
-    val secretCode = SecretCodeGenerator()
     if (easy) Printer(Output.revealSecretCode(secretCode))
     if (board.rounds.nonEmpty) {}
 
     Printer(Output.remainingAttempts(remainingAttempts))
     Printer(Output.promptForGuess)
+
+    val input: String = Reader.getInput
+    //val guess: Guess = PegContainerProducer(input)
   }
 }
