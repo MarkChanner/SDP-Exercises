f81a20eef2de8f79edf8a9b51c02aa0115a7315d
markchanner <markchanner@gmail.com>
2016-03-19 16:57:02 +0000

    Implemented output of remaining attempts

--- cw-two/src/main/scala/Mastermind.scala
+++ cw-two/src/main/scala/Mastermind.scala
@@ -1,9 +1,19 @@
 case class Mastermind(easy: Boolean) extends GameAbstractImpl {
+
+  val MAXATTEMPTS = 12
+
   def runGames = {
     val board = Board(Vector[Round]())
+    val remainingAttempts = MAXATTEMPTS - board.rounds.length
+
     Printer(Output.intro)
     Printer(Output.begin)
+
     val secretCode = SecretCodeGenerator()
     if (easy) Printer(Output.revealSecretCode(secretCode))
+    if (board.rounds.nonEmpty) {}
+
+    Printer(Output.remainingAttempts(remainingAttempts))
+
   }
 }
--- cw-two/src/main/scala/MastermindDriver.scala
+++ cw-two/src/main/scala/MastermindDriver.scala
@@ -1,8 +1,10 @@
 object MastermindDriver {
   def main(args: Array[String]) {
-    var g: Game = Factory.getInstance(classOf[Game], true)
+   // var g: Game = Factory.getInstance(classOf[Game], true)
+    var g: Game = Mastermind(true)
     g.runGames
-    g = Factory.getInstance(classOf[Game], false)
-    g.runGames
+    //g = Factory.getInstance(classOf[Game], false)
+    //g = Mastermind(false)
+    //g.runGames
   }
 }
