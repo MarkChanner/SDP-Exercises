48d561e90b9aa748efa8f8fd299ab19159d8acc6
markchanner <markchanner@gmail.com>
2016-03-19 14:26:57 +0000

    Added additional Round tests

--- cw-two/src/test/scala/RoundTest.scala
+++ cw-two/src/test/scala/RoundTest.scala
@@ -7,13 +7,15 @@ class RoundTest extends FunSuite with Matchers {
     val empty: Round = Round(guess1, feedback1)
     empty.toString should be("No pegs\n")
 
-    //val guess2 = Guess(Vector(Red(), Red()))
-    //val feedback2 = Feedback(Vector(Black()))
-    //val directMatch: Round = Round(guess2, feedback2)
-    //directMatch.toString should be("RR Result: Black\n")
+    val guess2 = Guess(Vector(Red(), Red()))
+    val feedback2 = Feedback(Vector(Black()))
+    val directMatch: Round = Round(guess2, feedback2)
+    directMatch.toString should be("RR Result: Black\n")
 
-    //val round3: Round = Round()
-    //round3.toString should be("Black White")
+    val guess3 = Guess(Vector(Red(), Red()))
+    val feedback3 = Feedback(Vector(White(), Black()))
+    val directAndIndirectMatch: Round = Round(guess3, feedback3)
+    directAndIndirectMatch.toString should be("RR Result: White Black\n")
   }
 }
 
