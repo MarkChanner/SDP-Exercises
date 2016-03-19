29d45ccc23dd379074d4228d3fdf657479e75cf4
markchanner <markchanner@gmail.com>
2016-03-19 16:30:29 +0000

    Initial implementation of concrete Game class 'Mastermind' with some starting outputs

--- cw-two/src/main/scala/Mastermind.scala
+++ cw-two/src/main/scala/Mastermind.scala
@@ -1,5 +1,8 @@
 case class Mastermind(easy: Boolean) extends GameAbstractImpl {
   def runGames = {
-
+    val board = Board(Vector[Round]())
+    Printer(Output.intro)
+    Printer(Output.begin)
+    val secretCode = SecretCodeGenerator()
   }
 }
