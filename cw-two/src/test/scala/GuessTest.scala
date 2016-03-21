16f58954244cfc908df322d459b21d6ce2e627da
markchanner <markchanner@gmail.com>
2016-03-21 17:53:02 +0000

    Adds GuessTest

--- cw-two/src/test/scala/GuessTest.scala
+++ cw-two/src/test/scala/GuessTest.scala
@@ -1,7 +1,14 @@
 import org.scalatest.{FunSuite, Matchers}
 
 class GuessTest extends FunSuite with Matchers {
-  test("Tests Guess") {
+  test("tests Guess") {
+    val guessPegs: Vector[GuessPeg] = Vector[GuessPeg](Orange(), Orange(), Blue(), Blue())
+    val emptyGuess: Vector[GuessPeg] = Vector[GuessPeg]()
 
+    val empty: Guess = Guess(emptyGuess)
+    empty.toString should be("")
+
+    val guess: Guess = Guess(guessPegs)
+    guess.toString should be("OOBB")
   }
 }
