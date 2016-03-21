577e3867131b37cc0bc97e465a82a2156f6003e5
markchanner <markchanner@gmail.com>
2016-03-21 19:02:45 +0000

    Implemented Validator apply method with accompanying valid and allValidLetters methods.

--- cw-two/src/main/scala/Validator.scala
+++ cw-two/src/main/scala/Validator.scala
@@ -1,3 +1,24 @@
 object Validator {
-  def apply(input: String): Input = ???
+
+  val guessLength = 4
+  val validPegs = "BGOPRY"
+
+  def apply(input: String): Input = {
+    if (valid(input)) Valid(input)
+    else Invalid(input)
+  }
+
+  def valid(input: String): Boolean = {
+    input.length == guessLength &&
+      (!input.exists(_.isLower)) &&
+      allValidLetters(input)
+  }
+
+  def allValidLetters(guess: String) = {
+    var result = true
+    for (letter <- guess) {
+      if (!validPegs.contains(letter)) result = false
+    }
+    result
+  }
 }
