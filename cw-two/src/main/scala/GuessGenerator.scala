7e18cb9baa17c95f28900c55bd39127a52ac41db
markchanner <markchanner@gmail.com>
2016-03-21 18:50:02 +0000

    Adds apply and run methods to recursively build a Guess object containing a Vector of pegs representing each input string

--- cw-two/src/main/scala/GuessGenerator.scala
+++ cw-two/src/main/scala/GuessGenerator.scala
@@ -1,5 +1,30 @@
 object GuessGenerator {
 
-  def apply(input: Input): Guess = ???
+  val guessLength = 4
 
+  def apply(input: Input): Guess = {
+    run(input.text, Vector[GuessPeg]())
+  }
+
+  def run(input: String, pegs: Vector[GuessPeg]): Guess = {
+    if (pegs.size == guessLength) Guess(pegs)
+    else {
+      var p = pegs
+      input.substring(0, 1) match {
+        case "B" =>
+          p = p :+ Blue()
+        case "G" =>
+          p = p :+ Green()
+        case "O" =>
+          p = p :+ Orange()
+        case "P" =>
+          p = p :+ Purple()
+        case "R" =>
+          p = p :+ Red()
+        case "Y" =>
+          p = p :+ Yellow()
+      }
+      run(input.substring(1), p)
+    }
+  }
 }
