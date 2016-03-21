c5b38ae2d1c2323dbb69457e025ab3bab055cdb3
markchanner <markchanner@gmail.com>
2016-03-21 20:08:11 +0000

    Adds FeedbackProducer, which has passed all tests

--- cw-two/src/main/scala/FeedbackProducer.scala
+++ cw-two/src/main/scala/FeedbackProducer.scala
@@ -1,5 +1,28 @@
+import util.Random._
+
 object FeedbackProducer {
 
-  def apply(guess: Guess, secretCode: SecretCode): Feedback = ???
+  def apply(guess: Guess, secretCode: SecretCode): Feedback = {
+    val zippedPegs = guess.pegs.zip(secretCode.pegs)
+
+    val blackPegs: Vector[ResultPeg] = zippedPegs
+      .filter(x => x._1 == x._2)
+      .map(x => Black())
+
+    val trimmedGuess: Vector[Peg] = zippedPegs
+      .filter(x => x._1 != x._2)
+      .map(x => x._1)
+
+    val trimmedSecretCode: Vector[Peg] = zippedPegs
+      .filter(x => x._1 != x._2)
+      .map(x => x._2)
+
+    val whitePegs: Vector[ResultPeg] = trimmedGuess.intersect(trimmedSecretCode)
+      .map(x => White())
+
+    val resultPegs: Vector[ResultPeg] = Vector(blackPegs, whitePegs).flatten //shuffle(Vector(blackPegs, whitePegs).flatten)
+
+    Feedback(resultPegs)
+  }
 
 }
