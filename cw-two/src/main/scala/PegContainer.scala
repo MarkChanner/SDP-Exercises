ad984685d02f161e38521f454028d49b0021fba2
markchanner <markchanner@gmail.com>
2016-03-21 17:14:27 +0000

    Moves toString for PegContainer subclasses up into PegContainer and uses pattern matching to call appropriate toString

--- cw-two/src/main/scala/PegContainer.scala
+++ cw-two/src/main/scala/PegContainer.scala
@@ -1,10 +1,17 @@
-trait PegContainer {
+sealed trait PegContainer {
+
   def pegs: Vector[Peg]
 
-  override def toString = pegs.mkString("")
+  override def toString =
+    this match {
+      case Feedback(pegs: Vector[ResultPeg]) => pegs.mkString(" ")
+      case Guess(pegs: Vector[GuessPeg]) => pegs.mkString("")
+      case SecretCode(pegs: Vector[GuessPeg]) => pegs.mkString("")
+    }
 }
 
-case class SecretCode(pegs: Vector[Peg]) extends PegContainer
+final case class Feedback(pegs: Vector[ResultPeg]) extends PegContainer
 
-case class Guess(pegs: Vector[Peg]) extends PegContainer
+final case class Guess(pegs: Vector[GuessPeg]) extends PegContainer
 
+final case class SecretCode(pegs: Vector[GuessPeg]) extends PegContainer
