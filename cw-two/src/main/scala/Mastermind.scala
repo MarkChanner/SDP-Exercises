98f43d9be00cc0f07f455c5dc9c9e455436f51ba
markchanner <markchanner@gmail.com>
2016-03-21 20:25:33 +0000

    starting to structure the game to give same as SampleOutput.txt

--- cw-two/src/main/scala/Mastermind.scala
+++ cw-two/src/main/scala/Mastermind.scala
@@ -1,22 +1,59 @@
 case class Mastermind(easy: Boolean) extends GameAbstractImpl {
-
+  val guessLength = 4
   val maxAttempts = 12
 
   def runGames = {
-    val board = Board(Vector[Round]())
-    val remainingAttempts = maxAttempts - board.rounds.length
-    val secretCode = SecretCodeGenerator()
+    var input: String = ""
+    var validInput: Input = Invalid("")
+    var looping = true
+    var counter = 12
+    var board = Board(Vector[Round]())
+    var latestRound: Round = Round(Guess(Vector[GuessPeg]()), Feedback(Vector[ResultPeg]()))
+    val secretCode = SecretCode(Vector[GuessPeg](Blue(), Green(), Blue(), Green())) //SecretCodeGenerator()
 
     Printer(Output.intro)
     Printer(Output.begin)
-
     if (easy) Printer(Output.revealSecretCode(secretCode))
-    if (board.rounds.nonEmpty) {}
 
-    Printer(Output.remainingAttempts(remainingAttempts))
-    Printer(Output.promptForGuess)
+    do {
+      //if (board.rounds.nonEmpty) {
+      if (counter != 12) { // temporary way of looping through game
+        do {
+          Printer(Output.promptForGuess)
+          input = Reader.getInput
+          validInput = Validator(input)
+        } while (!validInput.isValid)
 
-    val input: String = Reader.getInput
-    //val guess: Guess = PegContainerProducer(input)
+        println()
+        if (easy) Printer(Output.revealSecretCode(secretCode))
+
+        val guess: Guess = GuessGenerator(validInput)
+        val feedback: Feedback = FeedbackProducer(guess, secretCode)
+        latestRound = Round(guess, feedback)
+        board = Board(Vector[Round](latestRound))
+
+        Round.analyse(latestRound.feedback, guessLength) match {
+          case Solved =>
+            Printer(Output.secretCodeUnmasked(secretCode))
+            Printer(Output.roundFeedback(board))
+            Printer(Solved.toString)
+            Printer(Output.anotherGame)
+            //continuePlaying = Reader.getInput // ask if want another game
+            looping = false // temporarily ending loop automatically when Solved
+          case Unsolved =>
+            Printer(Output.secretCodeMasked)
+            Printer(Output.roundFeedback(board))
+            Printer(Output.fillerRows(maxAttempts - board.rounds.length))
+            if (board.rounds.length == maxAttempts) {
+              Printer(Unsolved.toString)
+              Printer(Output.anotherGame)
+              //continuePlaying = Reader.getInput // ask if want another game
+              looping = false // temporarily ending loop automatically when used all rounds
+            }
+        }
+      }
+      counter -= 1 // temporary
+      Printer(Output.remainingAttempts(maxAttempts - board.rounds.length))
+    } while (looping)
   }
 }
