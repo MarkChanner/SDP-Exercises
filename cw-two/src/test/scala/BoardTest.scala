b99176b60092d111f1f0a27b1182b50cc6a7cdc2
markchanner <markchanner@gmail.com>
2016-03-22 21:10:07 +0000

    Updates in preparation to test for Board returning a new version of itself with the most recent round added

--- cw-two/src/test/scala/BoardTest.scala
+++ cw-two/src/test/scala/BoardTest.scala
@@ -1,25 +1,48 @@
 import org.scalatest.{FunSuite, Matchers}
 
 class BoardTest extends FunSuite with Matchers {
+
   test("testResult") {
-    val guess1 = Guess(Vector(Red(), Red()))
+    var board = Board(Vector[Round]())
+
+    val guess1 = Guess(Vector[GuessPeg](Yellow(), Yellow(), Yellow(), Yellow()))
     val feedback1 = Feedback(Vector[ResultPeg]())
-    val empty: Round = Round(guess1, feedback1)
+    val round1 = Round(guess1, feedback1)
+    board = Board(board, round1)
+    board.toString should be("YYYY Result: No pegs\n")
 
-    val guess2 = Guess(Vector(Red(), Red()))
-    val feedback2 = Feedback(Vector(Black()))
-    val directMatch: Round = Round(guess2, feedback2)
+    val guess2 = Guess(Vector[GuessPeg](Red(), Yellow(), Yellow(), Yellow()))
+    val feedback2 = Feedback(Vector[ResultPeg](Black()))
+    val round2 = Round(guess2, feedback2)
+    board = Board(board, round2)
+    board.toString should be("YYYY Result: No pegs\n" +
+      "RYYY Result: Black\n")
 
-    val guess3 = Guess(Vector(Red(), Red()))
-    val feedback3 = Feedback(Vector(White(), Black()))
-    val directAndIndirectMatch: Round = Round(guess3, feedback3)
+    val guess3 = Guess(Vector[GuessPeg](Red(), Red(), Yellow(), Yellow()))
+    val feedback3 = Feedback(Vector[ResultPeg](Black(), Black()))
+    val round3 = Round(guess3, feedback3)
+    board = Board(board, round3)
+    board.toString should be("YYYY Result: No pegs\n" +
+      "RYYY Result: Black\n" +
+      "RRYY Result: Black Black\n")
 
-    val oneRound: Board = Board(Vector(empty))
-    oneRound.toString should be("RR Result: No pegs\n")
+    val guess4 = Guess(Vector[GuessPeg](Red(), Red(), Red(), Yellow()))
+    val feedback4 = Feedback(Vector[ResultPeg](Black(), Black(), Black()))
+    val round4 = Round(guess4, feedback4)
+    board = Board(board, round4)
+    board.toString should be("YYYY Result: No pegs\n" +
+      "RYYY Result: Black\n" +
+      "RRYY Result: Black Black\n" +
+      "RRRY Result: Black Black Black\n")
 
-    val threeRounds: Board = Board(Vector(empty, directMatch, directAndIndirectMatch))
-    threeRounds.toString should be("RR Result: No pegs\nRR Result: Black\nRR Result: White Black\n")
+    val guess5 = Guess(Vector[GuessPeg](Red(), Red(), Red(), Red()))
+    val feedback5 = Feedback(Vector[ResultPeg](Black(), Black(), Black(), Black()))
+    val round5 = Round(guess5, feedback5)
+    board = Board(board, round5)
+    board.toString should be("YYYY Result: No pegs\n" +
+      "RYYY Result: Black\n" +
+      "RRYY Result: Black Black\n" +
+      "RRRY Result: Black Black Black\n" +
+      "RRRR Result: Black Black Black Black\n")
   }
 }
-
-
