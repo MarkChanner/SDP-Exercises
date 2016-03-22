e923d769de95ef0fa47e4c9442a3ca64367ddd95
markchanner <markchanner@gmail.com>
2016-03-22 21:27:44 +0000

    Adds companion object to Board which returns a version of Board with latest round added

--- cw-two/src/main/scala/Board.scala
+++ cw-two/src/main/scala/Board.scala
@@ -1,17 +1,17 @@
-case class Board(rounds: Vector[Round]) {
-  override def toString() = {
+case class Board(val rounds: Vector[Round]) {
+  override def toString = {
     val output = StringBuilder.newBuilder
     rounds.map(r => output.append(r))
     output.toString
   }
-
 }
 
-object Main extends App {
-  val guess = Guess(Vector(Red(), Red()))
-  val feedback = Feedback(Vector(Black()))
-  val round = Round(guess, feedback)
-  val round2 = Round(guess, feedback)
-  val b = Board(Vector(round, round2))
-  print(b)
+object Board {
+  def apply: Board = {
+    Board(Vector[Round]())
+  }
+
+  def apply(b: Board, latestRound: Round): Board = {
+    Board(b.rounds :+ latestRound)
+  }
 }
